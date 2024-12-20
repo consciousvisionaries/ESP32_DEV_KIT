103513061168a3fdd941e27ac54fb8247ff27cab
consciousvisionaries <paul@consciousvisionaries.ca>
2024-12-19 19:16:23 -0800

    Update V1.3.5

--- ESP_FIRMWARE.ino
+++ ESP_FIRMWARE.ino
@@ -6,24 +6,20 @@ void saveWiFiCredentials(const String& newSSID, const String& newPassword, const
   preferences.clear(); // Clears all preferences in the "settings" namespace
   preferences.end();
   preferences.begin("settings", false);
-  
   Serial.println("Saving Version: " + newtxtVersion);
   // Store each piece separately
   preferences.putString("ssid", newSSID);
   preferences.putString("password", newPassword);
   preferences.putString("versiontxt", newtxtVersion);
   
-String savedVersion = preferences.getString("versiontxt", "");
-if (savedVersion == newtxtVersion) {
-    Serial.println("Version saved successfully." + savedVersion);
-} else {
-    Serial.println("Version save failed.");
-}
+  String savedVersion = preferences.getString("versiontxt", "");
+  if (savedVersion == newtxtVersion) {
+    Serial.println("    Version saved successfully." + savedVersion);
+  } else {
+    Serial.println("    Version save failed.");
+  }
 
-  
   preferences.end(); // Close namespace
-  
-  Serial.println("WiFi credentials and version saved.");
 }
 
 // Function to load WiFi credentials and version from Preferences
@@ -37,19 +33,15 @@ void loadWiFiCredentials() {
   preferences.end(); // Close namespace
   
   if (ssid.isEmpty() || password.isEmpty()) {
-    Serial.println("WiFi credentials not found.");
+    Serial.println("Loading WiFi... credentials not found.");
   } else {
     Serial.println("Loaded WiFi credentials: SSID=" + ssid + ", Password=" + password + ", Version=" + storedVersion);
   }
 }
 
-
-
-
-
 void connectWiFi() {
   Serial.printf("Connecting to WiFi: %s\n", ssid.c_str());
-  Serial.printf("WiFi Password: %s\n", password.c_str());
+  Serial.printf("    WiFi Password: %s\n", password.c_str());
   WiFi.begin(ssid.c_str(), password.c_str());
 
   unsigned long startAttemptTime = millis();
@@ -58,13 +50,13 @@ void connectWiFi() {
     Serial.print(".");
   }
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nWiFi connected.");
-    Serial.print("IP Address: ");
+    Serial.println("    \nWiFi connected.");
+    Serial.print("    IP Address: ");
     Serial.println(WiFi.localIP());
   } else {
-    Serial.println("\nWiFi connection failed. Starting Access Point...");
+    Serial.println("    \nWiFi connection failed. Starting Access Point...");
     WiFi.softAP("ESP32_AccessPoint", "12345678");
-    Serial.print("Access Point IP Address: ");
+    Serial.print("    Access Point IP Address: ");
     Serial.println(WiFi.softAPIP());
   }
 }
@@ -72,8 +64,10 @@ void connectWiFi() {
 void firmwareUpdates() {
   static unsigned long lastOTA = 0;
   if (millis() - lastOTA > 3600000) {  // Check for updates every hour
+    Serial.println("Last OTA > " + lastOTA);
     lastOTA = millis();
     checkForUpdates();
+    
   }
 }
 
@@ -88,25 +82,25 @@ void checkForUpdates() {
   if (httpCode == 200) {
     String newVersion = http.getString();
     newVersion.trim();
-    Serial.println("HTTP Response Code: " + String(httpCode));
-    Serial.println("Payload: " + newVersion);
+    Serial.println("    HTTP Response Code: " + String(httpCode));
+    Serial.println("    Payload: " + newVersion);
 
     if (newVersion != storedVersion) {
-      Serial.println("New version available: " + newVersion);
-      Serial.println("Updating firmware...");
+      Serial.println("    New version available: " + newVersion);
+      Serial.println("    Updating firmware...");
       if (updateFirmware(newVersion)) {
         saveWiFiCredentials(ssid, password, newVersion);
-        Serial.println("Update complete. Rebooting..."+ newVersion);
+        Serial.println("    Update complete. Rebooting..."+ newVersion);
         delay(5000);
         ESP.restart();
       } else {
-        Serial.println("Firmware update failed.");
+        Serial.println("    Firmware update failed.");
       }
     } else {
-      Serial.println("Device firmware is up-to-date.");
+      Serial.println("    Device firmware is up-to-date.");
     }
   } else {
-    Serial.println("Failed to fetch version info. HTTP code: " + String(httpCode));
+    Serial.println("    Failed to fetch version info. HTTP code: " + String(httpCode));
   }
   http.end();
 }
@@ -128,31 +122,31 @@ bool updateFirmware(const String& newVersion) {
       size_t written = Update.writeStream(*client);
 
       if (written == contentLength) {
-        Serial.println("Firmware written successfully.");
+        Serial.println("    Firmware written successfully.");
         
       } else {
-        Serial.println("Firmware write failed. Written: " + String(written) + ", Expected: " + String(contentLength));
+        Serial.println("    Firmware write failed. Written: " + String(written) + ", Expected: " + String(contentLength));
         return false;
       }
 
       if (Update.end()) {
         if (Update.isFinished()) {
-          Serial.println("Update successfully completed.");
+          Serial.println("    Update successfully completed.");
           return true;
         } else {
-          Serial.println("Update not finished. Something went wrong.");
+          Serial.println("    Update not finished. Something went wrong.");
           return false;
         }
       } else {
-        Serial.println("Update failed. Error #: " + String(Update.getError()));
+        Serial.println("    Update failed. Error #: " + String(Update.getError()));
         return false;
       }
     } else {
-      Serial.println("Not enough space for update.");
+      Serial.println("     Not enough space for update.");
       return false;
     }
   } else {
-    Serial.println("Failed to download firmware. HTTP code: " + String(httpCode));
+    Serial.println("    Failed to download firmware. HTTP code: " + String(httpCode));
     return false;
   }
   http.end();
