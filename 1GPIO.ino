dcfc24ee4e8732465c8cdac9600e0ef467ecba24
consciousvisionaries <paul@consciousvisionaries.ca>
2025-01-01 16:23:47 -0800

    Update v36

--- 1GPIO.ino
+++ 1GPIO.ino
@@ -2,10 +2,7 @@
 
 #define DEBOUNCE_DELAY 500  // Debounce delay in milliseconds
 
-const int outputPins[NUM_OUTPUTS] = {PIN_O1, PIN_O2, PIN_O3, PIN_O4, PIN_O5, PIN_O6, PIN_O7, PIN_O8};
-const int inputPins[NUM_INPUTS] = {PIN_B1, PIN_B2, PIN_B3, PIN_B4, PIN_B5, PIN_B6, PIN_B7, PIN_B8};
-//const int sortOrder[NUM_OUTPUTS] = {2,7,1,6,3,4,5,8};
-//const int outputState_default[NUM_OUTPUTS] = {0,0,0,1,0,0,0,0};
+
 
 String getInputStateGPIO(int inputPin) {
   int state = digitalRead(inputPin);
@@ -35,14 +32,14 @@ void toggleOutputStateGPIO(int outputNumber) {
 void setupGPIO() {
 
   for (int i = 0; i < NUM_OUTPUTS; i++) {
-    pinMode(outputPins[i], OUTPUT);  // Ensure the pin is set to OUTPUT
+    pinMode(outputPins[sortOrder[i]], OUTPUT);  // Ensure the pin is set to OUTPUT
     digitalWrite(outputPins[i], HIGH);
   }
   
   Serial.println("Outputs Initialized.");
 
   for (int i = 0; i < NUM_INPUTS; i++) {
-    pinMode(inputPins[i], INPUT_PULLUP);  // Ensure the pin is set to OUTPUT
+    pinMode(inputPins[sortOrder[i]], INPUT_PULLUP);  // Ensure the pin is set to OUTPUT
   }
   
   Serial.println("Inputs Initialized.");
